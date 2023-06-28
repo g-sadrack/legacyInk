7fe092d3e89b7046ab27c1a81e8dfba7cb81038c
sadrack404 <gabrielscr2023@gmail.com>
2023-06-28 11:53:50 -0300

    adicao do atributo agendamento eos metodos para marcar e desmarcar agendamento

--- src/main/java/br/com/legacyink/domain/model/Tatuador.java
+++ src/main/java/br/com/legacyink/domain/model/Tatuador.java
@@ -27,5 +27,26 @@ public class Tatuador {
     private List<Especialidade> especialidades = new ArrayList<>();
     private Integer tempoExperiencia;
     private BigDecimal avaliacao;
+    @OneToMany
+    @JoinTable(name = "agendamento_tatuadores",
+            joinColumns = @JoinColumn(name = "tatuador_id"),
+            inverseJoinColumns = @JoinColumn(name = "agendamento_id"))
+    private List<Agendamento> agendamento = new ArrayList<>();
+
+    public Tatuador(Long id, String nome, List<Especialidade> especialidades, Integer tempoExperiencia, BigDecimal avaliacao) {
+        this.id = id;
+        this.nome = nome;
+        this.especialidades = especialidades;
+        this.tempoExperiencia = tempoExperiencia;
+        this.avaliacao = avaliacao;
+    }
+
+    public void marcarAgendamento(Agendamento agendamento) {
+        this.agendamento.add(agendamento);
+    }
+
+    public void desmarcarAgendamento(Agendamento agendamento) {
+        this.agendamento.remove(agendamento);
+    }
 
 }
