6db953a79bd335963f73a4da589c2efdb4e50489
sadrack404 <gabrielscr2023@gmail.com>
2023-07-04 17:39:18 -0300

    alteracao de itemInout e itemDTO

--- src/main/java/br/com/legacyink/api/dto/ItemDTO.java
+++ src/main/java/br/com/legacyink/api/dto/ItemDTO.java
@@ -1,10 +1,14 @@
 package br.com.legacyink.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemDTO {
     private Long id;
     private String nome;
--- src/main/java/br/com/legacyink/api/dto/input/ItemInput.java
+++ src/main/java/br/com/legacyink/api/dto/input/ItemInput.java
@@ -1,10 +1,14 @@
 package br.com.legacyink.api.dto.input;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemInput {
     private String nome;
     private String descricao;
